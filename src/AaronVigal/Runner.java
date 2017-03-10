2c7d78f8b2c63b4ff05d1124e10b42a8dede2cc1
Aaron Vigal <admin@aaronvigal.com>
2017-03-10 12:53:36 -0600

    Added Runner and Package-info

---
+++ src/AaronVigal/Runner.java
@@ -0,0 +1,11 @@
+package AaronVigal;
+
+public class Runner {
+
+	public static void main(String[] args){
+		
+		
+		
+	}
+	
+}
---
+++ src/AaronVigal/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * @author Aaron Vigal
+ * @date March 10th
+ */
+
+package AaronVigal;
+
+/**
+ * An application designed as a proof of concept to attack Millard Public Schools logins that are known to be brute forcible. 
+ * The goal is to bring attention to the fact that our accounts are at risk and action should be taken to prevent this painstakingly easy attack. 
+ * We have curated a list of usernames exported from Google Contacts and programatically generated a list of possible password combinations from the formula used to generate passwords for Millard's Students.
+ */
